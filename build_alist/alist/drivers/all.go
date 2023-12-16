f85f2c51f1d4512b2bf3858b424bf9306fb20a36
k930520 <@k930520>
2023-12-16 23:14:23 +0800

    upload file

---
+++ build_alist/alist/drivers/all.go
@@ -0,0 +1,10 @@
+package drivers
+
+import (
+	_ "github.com/alist-org/alist/v3/drivers/139"
+	_ "github.com/alist-org/alist/v3/drivers/alist_v3"
+	_ "github.com/alist-org/alist/v3/drivers/aliyundrive_open"
+	_ "github.com/alist-org/alist/v3/drivers/aliyundrive_share"
+	_ "github.com/alist-org/alist/v3/drivers/baidu_photo"
+	_ "github.com/alist-org/alist/v3/drivers/crypt"
+)
