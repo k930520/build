f822c9a2c24510cb00ca41d25de427a096385a2c
k930520 <42566402+k930520@users.noreply.github.com>
2024-12-21 19:52:42 +0800

    Update build.sh

--- build_hi3798/build.sh
+++ build_hi3798/build.sh
@@ -2,8 +2,7 @@ cp build_hi3798/emmc_bootargs.txt SDK/configs/hi3798mv100/prebuilts/emmc_bootarg
 cp build_hi3798/emmc_partitions.xml SDK/configs/hi3798mv100/prebuilts/emmc_partitions.xml
 mkdir compress
 cd SDK
-sh server_install.sh
-cp configs/hi3798mv100/hi3798mdmo1g_hi3798mv100_cfg.mak ./cfg.mak
 source ./env.sh
+cp configs/hi3798mv100/hi3798mdmo1g_hi3798mv100_cfg.mak ./cfg.mak
 make prebuilts
 tar -czvf ../compress/hi3798.tar.gz ./out/hi3798mv100/hi3798mdmo1g/image/emmc_image/*
