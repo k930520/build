6654156602247d7aee48cdb79e6c13671fb7ad2f
k930520 <42566402+k930520@users.noreply.github.com>
2024-12-21 22:10:34 +0800

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+set -eo pipefail
+
+# A POSIX variable
+OPTIND=1 # Reset in case getopts has been used previously in the shell.
+
+while getopts "a:v:q:u:d:s:i:o:" opt; do
+    case "$opt" in
+    a)  ARCH=$OPTARG
+        ;;
+    v)  VERSION=$OPTARG
+        ;;
+    q)  QEMU_ARCH=$OPTARG
+        ;;
+    u)  QEMU_VER=$OPTARG
+        ;;
+    d)  DOCKER_REPO=$OPTARG
+        ;;
+    s)  SUITE=$OPTARG
+        ;;
+    i)  INCLUDE=$OPTARG
+        ;;
+    o)  UNAME_ARCH=$OPTARG
+        ;;
+    esac
+done
+
+shift $((OPTIND-1))
+
+[ "$1" = "--" ] && shift
+
+dir="$VERSION"
+COMPONENTS="main"
+VARIANT="minbase"
+args=( -d "$dir" debootstrap --variant="$VARIANT" --components="$COMPONENTS" --include="$INCLUDE" --arch="$ARCH" "$SUITE" )
+
+mkdir -p mkimage $dir
+curl https://raw.githubusercontent.com/moby/moby/6f78b438b88511732ba4ac7c7c9097d148ae3568/contrib/mkimage.sh > mkimage.sh
+curl https://raw.githubusercontent.com/moby/moby/6f78b438b88511732ba4ac7c7c9097d148ae3568/contrib/mkimage/debootstrap > mkimage/debootstrap
+chmod +x mkimage.sh mkimage/debootstrap
+
+mkimage="$(readlink -f "${MKIMAGE:-"mkimage.sh"}")"
+{
+    echo "$(basename "$mkimage") ${args[*]/"$dir"/.}"
+    echo
+    echo 'https://github.com/moby/moby/blob/6f78b438b88511732ba4ac7c7c9097d148ae3568/contrib/mkimage.sh'
+} > "$dir/build-command.txt"
+
+sudo DEBOOTSTRAP="qemu-debootstrap" nice ionice -c 3 "$mkimage" "${args[@]}" 2>&1 | tee "$dir/build.log"
+cat "$dir/build.log"
+sudo chown -R "$(id -u):$(id -g)" "$dir"
+
+xz -d < $dir/rootfs.tar.xz | gzip -c > $dir/rootfs.tar.gz
