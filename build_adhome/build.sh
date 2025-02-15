3b39f3ee5d011b57151ed56fbec19bcbdf0619ae
k930520 <42566402+k930520@users.noreply.github.com>
2025-02-15 16:54:54 +0800

    Update build.sh

--- build_adhome/build.sh
+++ build_adhome/build.sh
@@ -1,14 +1,16 @@
-rm -rf AdGuardHome
+#rm -rf AdGuardHome
 
-version=$(wget -qO- -t1 -T2 "https://api.github.com/repos/AdguardTeam/AdGuardHome/releases/latest" | grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g')
+#version=$(wget -qO- -t1 -T2 "https://api.github.com/repos/AdguardTeam/AdGuardHome/releases/latest" | grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g')
 
-git clone -b $version https://github.com/AdguardTeam/AdGuardHome
+#git clone -b $version https://github.com/AdguardTeam/AdGuardHome
 
 sudo sed -i -e '/"slices"/a\ 	"strings"' AdGuardHome/internal/updater/check.go
 
 sudo sed -i -e "/return dlURL, key, true/r build_adhome/adhome/internal/updater/check.txt" -e "//d" AdGuardHome/internal/updater/check.go
 
 sudo sed -i -e '/return stringutil.FilterOut(conf.UpstreamDNS, IsCommentOrEmpty), nil/{s/.*/		upstreams = conf.UpstreamDNS } else {/;n;d;}' AdGuardHome/internal/dnsforward/config.go
+
+sudo sed -i -e '/if withECS {/{s/.*/	c.itemsWithSubnet = c.items/;n;d;n;d;}' $GOMODCACHE/github.com/!adguard!team/dnsproxy@v0.75.0/proxy/cache.go
 
 sudo sed -i -e "/return stringutil.FilterOut(upstreams, IsCommentOrEmpty), nil/r build_adhome/adhome/internal/dnsforward/config_u.txt" -e "//d" AdGuardHome/internal/dnsforward/config.go
 
@@ -22,9 +24,9 @@ sudo sed -i -e "/cv := newUpstreamConfigValidator(req.Upstreams, req.FallbackDNS
 
 cd AdGuardHome
 
-make CHANNEL='release' GOOS='linux' GOARCH='arm' GOARM='7' OUT='./dist/AdGuardHome/AdGuardHome'
+make CHANNEL='edge' GOOS='linux' GOARCH='arm' GOARM='7' OUT='./dist/AdGuardHome/AdGuardHome'
 
-tar -czvf dist/AdGuardHome_dist.tar.gz internal/*
+#tar -czvf dist/AdGuardHome_dist.tar.gz internal/*
 
 upx -9 ./dist/AdGuardHome/AdGuardHome
 
