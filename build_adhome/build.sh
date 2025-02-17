b0cdcad4e360d32c3e2f71841efedac2682f2b44
k930520 <42566402+k930520@users.noreply.github.com>
2025-02-18 02:10:57 +0800

    Update build.sh

--- build_adhome/build.sh
+++ build_adhome/build.sh
@@ -1,75 +1,78 @@
 BuildAdGuardHome() {
-	sudo sed -i '/"slices"/a\ 	"strings"' AdGuardHome/internal/updater/check.go
+sudo sed -i '/"slices"/a\ 	"strings"' AdGuardHome/internal/updater/check.go
 
-	sudo sed -i '/return dlURL, key, true/i\
- 		split := strings.Split(dlURL, "/")\
-		dlURL = "http://github.home.local/https://github.com/k930520/build/releases/download/adhome/" + strings.Replace(split[len(split)-1], "AdGuardHome", "AdGuardHome_"+u.channel, 1)' AdGuardHome/internal/updater/check.go
-  
-	cd AdGuardHome
+sudo sed -i '/return dlURL, key, true/i\
+	split := strings.Split(dlURL, "/")\
+	dlURL = "http://github.home.local/https://github.com/k930520/build/releases/download/adhome/" + strings.Replace(split[len(split)-1], "AdGuardHome", "AdGuardHome_"+u.channel, 1)' AdGuardHome/internal/updater/check.go
 
-	go mod tidy
+cd AdGuardHome
 
-	dnsproxy=$(ls /home/runner/go/pkg/mod/github.com/\!adguard\!team | grep dnsproxy)
+go mod tidy
 
- 	echo dnsproxy is $dnsproxy
+dnsproxy=$(ls /home/runner/go/pkg/mod/github.com/\!adguard\!team | grep dnsproxy)
 
-	sudo sed -i '/if withECS {/d' /home/runner/go/pkg/mod/github.com/\!adguard\!team/$dnsproxy/proxy/cache.go
+echo dnsproxy is $dnsproxy
 
-	sudo sed -i -e '/c.itemsWithSubnet = createCache(size)/{s/.*/	c.itemsWithSubnet = c.items/;n;d;}' /home/runner/go/pkg/mod/github.com/\!adguard\!team/$dnsproxy/proxy/cache.go
+sudo sed -i '/if withECS {/d' /home/runner/go/pkg/mod/github.com/\!adguard\!team/$dnsproxy/proxy/cache.go
 
- 	sudo sed -i '/"slices"/a\ 	"strings"' /home/runner/go/pkg/mod/github.com/\!adguard\!team/$dnsproxy/proxy/proxy.go
+sudo sed -i -e '/c.itemsWithSubnet = createCache(size)/{s/.*/	c.itemsWithSubnet = c.items/;n;d;}' /home/runner/go/pkg/mod/github.com/\!adguard\!team/$dnsproxy/proxy/cache.go
 
-        sudo sed -i '/dctx.calcFlagsAndSize()/i\		
-	for fqdn := dctx.Req.Question[0].Name; fqdn != ""; {\
-		ip, ok := p.UpstreamConfig.DomainEDNSAddr["*."+fqdn]\
-		if ok && fqdn != dctx.Req.Question[0].Name {\
-			dctx.processECS(ip.AsSlice(), p.logger)\
-			break\
-		}\
-		ip, ok = p.UpstreamConfig.DomainEDNSAddr[fqdn]\
-		if ok {\
-			dctx.processECS(ip.AsSlice(), p.logger)\
-			break\
-		}\
-		_, fqdn, _ = strings.Cut(fqdn, ".")\
-	}' /home/runner/go/pkg/mod/github.com/\!adguard\!team/$dnsproxy/proxy/proxy.go
+sudo sed -i '/"slices"/a\ 	"strings"' /home/runner/go/pkg/mod/github.com/\!adguard\!team/$dnsproxy/proxy/proxy.go
+	
+sudo sed -i '/dctx\.calcFlagsAndSize()/i\
+	for fqdn := dctx.Req.Question.Name; fqdn != ""; {\
+	    ip, ok := p.UpstreamConfig.DomainEDNSAddr["*."+fqdn]\
+	    if ok && fqdn != dctx.Req.Question.Name {\
+	        dctx.processECS(ip.AsSlice(), p.logger)\
+	        break\
+	    }\
+	    ip, ok = p.UpstreamConfig.DomainEDNSAddr[fqdn]\
+	    if ok {\
+	        dctx.processECS(ip.AsSlice(), p.logger)\
+	        break\
+	    }\
+	    _, fqdn, _ = strings.Cut(fqdn, ".")\
+	}\
+' /home/runner/go/pkg/mod/github.com/\!adguard\!team/$dnsproxy/proxy/proxy.go
 
-   	sudo sed -i '/"maps"/a\ 	"net/netip"' /home/runner/go/pkg/mod/github.com/\!adguard\!team/$dnsproxy/proxy/upstreams.go
+sudo sed -i '/"maps"/a\ 	"net/netip"' /home/runner/go/pkg/mod/github.com/\!adguard\!team/$dnsproxy/proxy/upstreams.go
 
-     	sudo sed -i '/type UpstreamConfig struct {/a\	DomainEDNSAddr map[string]netip.Addr' /home/runner/go/pkg/mod/github.com/\!adguard\!team/$dnsproxy/proxy/upstreams.go
-      
-     	sudo sed -i '/logger \*slog.Logger/a\	domainEDNSAddr map[string]netip.Addr' /home/runner/go/pkg/mod/github.com/\!adguard\!team/$dnsproxy/proxy/upstreams.go
-      
-	sudo sed -i '/logger:                   opts.Logger,/a\		domainEDNSAddr:           map[string]netip.Addr{},' /home/runner/go/pkg/mod/github.com/\!adguard\!team/$dnsproxy/proxy/upstreams.go
+sudo sed -i '/type UpstreamConfig struct {/a\	DomainEDNSAddr map[string]netip.Addr' /home/runner/go/pkg/mod/github.com/\!adguard\!team/$dnsproxy/proxy/upstreams.go
 
-	sudo sed -i '/Upstreams:                p.upstreams,/a\		DomainEDNSAddr:           p.domainEDNSAddr,' /home/runner/go/pkg/mod/github.com/\!adguard\!team/$dnsproxy/proxy/upstreams.go
+sudo sed -i '/logger \*slog.Logger/a\	domainEDNSAddr map[string]netip.Addr' /home/runner/go/pkg/mod/github.com/\!adguard\!team/$dnsproxy/proxy/upstreams.go
 
-   	sudo sed -i '/if confHost == "" {/i\		
+sudo sed -i '/logger:                   opts.Logger,/a\		domainEDNSAddr:           map[string]netip.Addr{},' /home/runner/go/pkg/mod/github.com/\!adguard\!team/$dnsproxy/proxy/upstreams.go
+
+sudo sed -i '/Upstreams:                p.upstreams,/a\		DomainEDNSAddr:           p.domainEDNSAddr,' /home/runner/go/pkg/mod/github.com/\!adguard\!team/$dnsproxy/proxy/upstreams.go
+
+sudo sed -i '/for _, confHost := range strings.Split(domainsLine, "\/") {/a\
 		confHost, ednsAddr, f := strings.Cut(confHost, "|")\
 		if f && ednsAddr == "" {\
-			return nil, nil, errors.Error("wrong upstream format")\
-		}' /home/runner/go/pkg/mod/github.com/\!adguard\!team/$dnsproxy/proxy/upstreams.go
+			return nil, nil, errors.New("wrong upstream format")\
+		}\
+' /home/runner/go/pkg/mod/github.com/\!adguard\!team/$dnsproxy/proxy/upstreams.go
 
-     	sudo sed -i '/domains = append(domains, strings.ToLower(confHost+"."))/i\		
+sudo sed -i '/domains = append(domains, strings.ToLower(confHost+"\."))/i\
 		addr, err := netip.ParseAddr(ednsAddr)\
 		if err != nil {\
 			return nil, nil, err\
 		}\
-		p.domainEDNSAddr[strings.ToLower(confHost+".")] = addr' /home/runner/go/pkg/mod/github.com/\!adguard\!team/$dnsproxy/proxy/upstreams.go
+		p.domainEDNSAddr[strings.ToLower(confHost+".")] = addr\
+' /home/runner/go/pkg/mod/github.com/\!adguard\!team/$dnsproxy/proxy/upstreams.go
 
-	make CHANNEL=$1 GOOS=linux GOARCH=arm GOARM=7 OUT=dist/AdGuardHome/AdGuardHome
+make CHANNEL=$1 GOOS=linux GOARCH=arm GOARM=7 OUT=dist/AdGuardHome/AdGuardHome
 
-	upx -9 dist/AdGuardHome/AdGuardHome
+upx -9 dist/AdGuardHome/AdGuardHome
 
-	tar -C "dist" -c -f - "./AdGuardHome" | gzip -9 - > "../build/AdGuardHome_$1_linux_armv7.tar.gz"
-	
-	cd ../
+tar -C "dist" -c -f - "./AdGuardHome" | gzip -9 - > "../build/AdGuardHome_$1_linux_armv7.tar.gz"
 
-	echo clean for $1
-	
-	rm -rf AdGuardHome
+cd ../
 
-  	go clean -modcache
+echo clean for $1
+
+rm -rf AdGuardHome
+
+go clean -modcache
 }
 
 mkdir build
