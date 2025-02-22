35f2bbe6e1f3a622198f322a748c7c066a38a253
k930520 <42566402+k930520@users.noreply.github.com>
2025-02-22 16:00:07 +0800

    Update build.sh

--- build_adhome/build.sh
+++ build_adhome/build.sh
@@ -37,7 +37,15 @@ sudo sed -i '/dctx\.calcFlagsAndSize()/i\
 		}\
 		_, fqdn, _ = strings.Cut(fqdn, ".")\
 	}\
-' /home/runner/go/pkg/mod/github.com/\!adguard\!team/$dnsproxy/proxy/proxy.go
+ ' /home/runner/go/pkg/mod/github.com/\!adguard\!team/$dnsproxy/proxy/proxy.go
+
+sudo sed -i -e '/if p.Config.EnableEDNSClientSubnet && d.ReqECS != nil {/i\
+	if d.hasEDNS0 && d.ReqECS != nil {\
+ ' -e '//d' /home/runner/go/pkg/mod/github.com/\!adguard\!team/$dnsproxy/proxy/proxycache.go
+
+sudo sed -i -e '/if !p.EnableEDNSClientSubnet {/i\
+	if !d.hasEDNS0 {\
+ ' -e '//d' /home/runner/go/pkg/mod/github.com/\!adguard\!team/$dnsproxy/proxy/proxycache.go
 
 sudo sed -i '/"maps"/a\ 	"net/netip"' /home/runner/go/pkg/mod/github.com/\!adguard\!team/$dnsproxy/proxy/upstreams.go
 
@@ -58,7 +66,7 @@ sudo sed -i '/for _, confHost := range strings.Split(domainsLine, "\/") {/a\
 		if fond && (confHost == "" || addrStr == "") {\
 			return nil, nil, errors.New("wrong upstream format")\
 		}\
-' /home/runner/go/pkg/mod/github.com/\!adguard\!team/$dnsproxy/proxy/upstreams.go
+  ' /home/runner/go/pkg/mod/github.com/\!adguard\!team/$dnsproxy/proxy/upstreams.go
 
 sudo sed -i '/domains = append(domains, strings.ToLower(confHost+"\."))/i\
 		addr, err := netip.ParseAddr(addrStr)\
@@ -66,7 +74,7 @@ sudo sed -i '/domains = append(domains, strings.ToLower(confHost+"\."))/i\
 			return nil, nil, err\
 		}\
 		p.domainEDNSAddr[strings.ToLower(confHost+".")] = addr\
-' /home/runner/go/pkg/mod/github.com/\!adguard\!team/$dnsproxy/proxy/upstreams.go
+  ' /home/runner/go/pkg/mod/github.com/\!adguard\!team/$dnsproxy/proxy/upstreams.go
 
 make CHANNEL=$1 GOOS=linux GOARCH=arm GOARM=7 OUT=dist/AdGuardHome/AdGuardHome
 
