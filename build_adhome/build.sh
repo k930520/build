d3d3a7d01914e79c1f0b680818f64445b22ecf15
k930520 <42566402+k930520@users.noreply.github.com>
2025-02-28 00:16:47 +0800

    Update build.sh

--- build_adhome/build.sh
+++ build_adhome/build.sh
@@ -129,6 +129,7 @@ sudo sed -i '/resp, u, err := p\.exchangeUpstreams(req, wrapped)/a\
 sudo sed -i '/unwrapped, stats := collectQueryStats(p\.UpstreamMode, u, wrapped, wrappedFallbacks)/i\
 	if p.AAAADisabled && req.Question[0].Qtype == dns.TypeAAAA {\
 		req.Question[0].Qtype = dns.TypeA\
+  		resp.Question[0].Qtype = dns.TypeA\
 	}\
 ' /home/runner/go/pkg/mod/github.com/\!adguard\!team/$dnsproxy/proxy/proxy.go
 
