990a6dd907a34ea9d799fd6a9f37eea036c1ee5c
k930520 <42566402+k930520@users.noreply.github.com>
2025-02-27 14:16:42 +0800

    Update meta.go

--- build_alist/alist/drivers/aliyundrive_share/meta.go
+++ build_alist/alist/drivers/aliyundrive_share/meta.go
@@ -12,9 +12,9 @@ type Addition struct {
 	OauthTokenURL    string `json:"oauth_token_url" default:"https://api.nn.ci/alist/ali_open/token"`
 	ClientID         string `json:"client_id" required:"false" help:"Keep it empty if you don't have one"`
 	ClientSecret     string `json:"client_secret" required:"false" help:"Keep it empty if you don't have one"`
-	Shares           string `json:"shares" required:"true" type:"text"`
 	RefreshToken     string `json:"refresh_token" required:"true"`
 	OpenRefreshToken string `json:"open_refresh_token" required:"true"`
+	Shares           string `json:"shares" required:"true" type:"text"`
 }
 
 var config = driver.Config{
