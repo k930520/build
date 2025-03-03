6b56874aae9e70617702f1b6adfd616d14fac5cf
k930520 <42566402+k930520@users.noreply.github.com>
2025-03-03 13:30:49 +0800

    Update build.sh

--- build_adhome/build.sh
+++ build_adhome/build.sh
@@ -158,8 +158,11 @@ sudo sed -i '/ci, expired, key = dctxCache\.getWithSubnet(d\.Req, d\.ReqECS)/a\
 		if ci == nil && p.AAAAEnabled && d.Req.Question[0].Qtype == dns.TypeA {\
 			d.Req.Question[0].Qtype = dns.TypeAAAA\
 			ci, expired, key = dctxCache.getWithSubnet(d.Req, d.ReqECS)\
-			if ci == nil {\
-				d.Req.Question[0].Qtype = dns.TypeA\
+			d.Req.Question[0].Qtype = dns.TypeA\
+			if ci != nil {\
+				d.Res = (&dns.Msg{}).SetRcode(d.Req, dns.RcodeSuccess)\
+				d.Res.RecursionAvailable = true\
+				return true\
 			}\
 		}\
 ' /home/runner/go/pkg/mod/github.com/\!adguard\!team/$dnsproxy/proxy/proxycache.go
@@ -168,8 +171,11 @@ sudo sed -i '/ci, expired, key = dctxCache\.get(d\.Req)/a\
 		if ci == nil && p.AAAAEnabled && d.Req.Question[0].Qtype == dns.TypeA {\
 			d.Req.Question[0].Qtype = dns.TypeAAAA\
 			ci, expired, key = dctxCache.get(d.Req)\
-			if ci == nil {\
-				d.Req.Question[0].Qtype = dns.TypeA\
+			d.Req.Question[0].Qtype = dns.TypeA\
+			if ci != nil {\
+				d.Res = (&dns.Msg{}).SetRcode(d.Req, dns.RcodeSuccess)\
+				d.Res.RecursionAvailable = true\
+				return true\
 			}\
 		}\
 ' /home/runner/go/pkg/mod/github.com/\!adguard\!team/$dnsproxy/proxy/proxycache.go
