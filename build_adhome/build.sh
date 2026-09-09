b286d69c4b2e10d37880e50efc3b1fdb0b4a1b8f
k930520 <42566402+k930520@users.noreply.github.com>
2026-09-09 16:17:15 +0800

    Limit CHANNEL to only 'edge' in build.sh
    Removed 'beta' and 'release' from CHANNEL array in build script.

--- build_adhome/build.sh
+++ build_adhome/build.sh
@@ -231,7 +231,7 @@ go clean -modcache
 
 mkdir build
 
-CHANNEL=(edge beta release)
+CHANNEL=(edge)
 for i in "${CHANNEL[@]}"; do
 	echo building for ${i}
 	version=$(wget -qO- -t1 -T2 "https://static.adtidy.org/adguardhome/${i}/version.json" | grep "version" | head -n 1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g')
