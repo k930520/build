75ba1b8f1f0b998f9901e6a494b28152744575c5
k930520 <42566402+k930520@users.noreply.github.com>
2026-09-18 19:07:54 +0800

    Create handle.go

---
+++ build_adhome/AdGuardHome/internal/transport/handle.go
@@ -0,0 +1,29 @@
+package transport
+
+import (
+	"io"
+	"net/http"
+
+	"github.com/AdguardTeam/golibs/logutil/slogutil"
+)
+
+func (t *Transport) HandleRequest(w http.ResponseWriter, r *http.Request) {
+	logger := t.logger.With(slogutil.KeyPrefix, "handle")
+	resp, err := t.RoundTrip(r)
+	if err != nil {
+		logger.ErrorContext(r.Context(), "transport roundtrip is failed", slogutil.KeyError, err)
+		http.Error(w, "transport roundtrip is failed", http.StatusBadRequest)
+		return
+	}
+	defer func(Body io.ReadCloser) {
+		_ = Body.Close()
+	}(resp.Body)
+	for key, values := range resp.Header {
+		for _, value := range values {
+			w.Header().Add(key, value)
+		}
+	}
+	w.WriteHeader(resp.StatusCode)
+	_, _ = io.Copy(w, resp.Body)
+	return
+}
