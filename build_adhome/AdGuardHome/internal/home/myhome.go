a8db5c91d5c96023b653ec65d233fcb31e92c271
k930520 <42566402+k930520@users.noreply.github.com>
2026-09-19 19:48:37 +0800

    Update myhome.go

--- build_adhome/AdGuardHome/internal/home/myhome.go
+++ build_adhome/AdGuardHome/internal/home/myhome.go
@@ -60,9 +60,6 @@ func (web *webAPI) myServeTLS(ctx context.Context) (next bool) {
 func (web *webAPI) myWrapMux(l *slog.Logger) (h http.Handler) {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if r.Host != web.tlsManager.ExtendedTLSConfig().ServerName {
-			if globalContext.dnsServer.Transport == nil {
-				globalContext.dnsServer.Transport = transport.NewTransport(l)
-			}
 			logMw := httputil.NewLogMiddleware(l, slog.LevelDebug)
 			h = logMw.Wrap(http.HandlerFunc(globalContext.dnsServer.HandleRequest))
 		} else {
