aa2613e2c89166e477eab4765f05775deaf9af89
k930520 <42566402+k930520@users.noreply.github.com>
2026-09-22 18:55:45 +0800

    Update build_edge.sh

--- build_adhome/build_edge.sh
+++ build_adhome/build_edge.sh
@@ -10,7 +10,7 @@ sudo sed -i '/type DefaultManager struct {/a\
 	aghaTLSOpt\
 ' AdGuardHome/internal/aghtls/defaultmanager.go
 
-sudo sed -i '	if conf\.ExtendedTLSConfig \!= nil {/i\
+sudo sed -i '	if conf\.ExtendedTLSConfig != nil {/i\
 	if conf.BindHosts != nil {\
 		mgr.bindHosts = conf.BindHosts\
 	}\
