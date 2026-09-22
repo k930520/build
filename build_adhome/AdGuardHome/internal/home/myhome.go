867af2b33e4bac6f2ee25e9d1b632589a1ffb4d8
k930520 <42566402+k930520@users.noreply.github.com>
2026-09-22 18:22:52 +0800

    Update myhome.go

--- build_adhome/AdGuardHome/internal/home/myhome.go
+++ build_adhome/AdGuardHome/internal/home/myhome.go
@@ -4,11 +4,14 @@ import (
 	"context"
 	"fmt"
 	"log/slog"
+	"net"
 	"net/http"
-	
+	"net/netip"
+
 	"github.com/AdguardTeam/golibs/errors"
 	"github.com/AdguardTeam/golibs/netutil"
 	"github.com/AdguardTeam/golibs/netutil/httputil"
+	"github.com/AdguardTeam/golibs/netutil/urlutil"
 )
 
 func (web *webAPI) myServeTLS(ctx context.Context) (next bool) {
@@ -16,20 +19,11 @@ func (web *webAPI) myServeTLS(ctx context.Context) (next bool) {
 		return false
 	}
 
-	var addr string
-	func() {
-		config.RLock()
-		defer config.RUnlock()
-
-		addr = netutil.JoinHostPort(config.TLS.ServerName, config.TLS.PortHTTPS)
-	}()
-
 	logger := web.baseLogger.With(loggerKeyServer, "https")
 
 	hdlr := web.myWrapMux(logger)
 
 	web.httpsServer.server = &http.Server{
-		Addr:              addr,
 		Handler:           hdlr,
 		TLSConfig:         web.tlsManager.TLSConfig(),
 		ReadTimeout:       web.conf.ReadTimeout,
@@ -38,19 +32,40 @@ func (web *webAPI) myServeTLS(ctx context.Context) (next bool) {
 		ErrorLog:          slog.NewLogLogger(logger.Handler(), slog.LevelError),
 	}
 
-	extTLSConf := web.tlsManager.ExtendedTLSConfig()
-	printHTTPSAddresses(ctx, web.logger, extTLSConf)
+	var bindHosts []netip.Addr
+	var portHTTPS uint16
+	func() {
+		config.RLock()
+		defer config.RUnlock()
 
-	if web.conf.serveHTTP3 {
-		go web.mustStartHTTP3(ctx, addr)
-	}
+		bindHosts = config.DNS.BindHosts
+		portHTTPS = config.TLS.PortHTTPS
+	}()
+	for _, host := range bindHosts {
+		addr := netutil.JoinHostPort(host.String(), portHTTPS)
 
-	logger.InfoContext(ctx, "starting https server")
-	err := web.httpsServer.server.ListenAndServeTLS("", "")
-	if !errors.Is(err, http.ErrServerClosed) {
-		cleanupAlways(ctx, logger, web.pidFilePath)
+		if web.conf.serveHTTP3 {
+			go web.mustStartHTTP3(ctx, addr)
+		}
 
-		panic(fmt.Errorf("https: %w", err))
+		ln, err := net.Listen("tcp", addr)
+		if err != nil {
+			cleanupAlways(ctx, logger, web.pidFilePath)
+
+			panic(fmt.Errorf("tcp listener: %w", err))
+		}
+
+		printWebAddrs(ctx, web.logger, urlutil.SchemeHTTPS, host.String(), portHTTPS)
+
+		go func() {
+			logger.InfoContext(ctx, "starting https server")
+			err = web.httpsServer.server.ServeTLS(ln, "", "")
+			if !errors.Is(err, http.ErrServerClosed) {
+				cleanupAlways(ctx, logger, web.pidFilePath)
+
+				panic(fmt.Errorf("https: %w", err))
+			}
+		}()
 	}
 
 	return true
@@ -58,11 +73,11 @@ func (web *webAPI) myServeTLS(ctx context.Context) (next bool) {
 
 func (web *webAPI) myWrapMux(l *slog.Logger) (h http.Handler) {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if r.Host != web.tlsManager.ExtendedTLSConfig().ServerName {
+		if netutil.IsValidIPString(r.Host) || r.Host == web.tlsManager.ExtendedTLSConfig().ServerName {
+			h = web.wrapMux(l)
+		} else {
 			logMw := httputil.NewLogMiddleware(l, slog.LevelDebug)
 			h = logMw.Wrap(http.HandlerFunc(globalContext.dnsServer.HandleRequest))
-		} else {
-			h = web.wrapMux(l)
 		}
 		h.ServeHTTP(w, r)
 	})
